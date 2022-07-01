86d1857409022fa684d7fd6f64b2308c51e649ba
Rebecca <t820134@spark.co.nz>
2022-07-01 23:22:45 +1200

    test

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const jsonfile = require("jsonfile");
 const moment = require("moment");
 const simpleGit = require("simple-git");
+const random = require("random");
 
 const FILE_PATH = "./data.json";
 
-async function addCommit() {
-	for (let i = 1; i < 180; i++) {
-		const DATE = moment().subtract(180, "d").add(i, "d").format();
-		const data = {
-			date: `${DATE + i} `,
-		};
-		for (let j = 0; j < Math.random() * 7; j++) {
-			await jsonfile.writeFile(FILE_PATH, data);
-			await simpleGit()
-				.add([FILE_PATH])
-				.commit(`${DATE + i} `, { "--date": DATE });
-		}
-	}
-}
+const addCommit = (numberOfCommits) => {
+	if (numberOfCommits === 0) return simpleGit().push();
+	const x = random.int(0, 27);
+	const y = random.int(0, 6);
 
-addCommit();
-// jsonfile.writeFile(FILE_PATH, data);
+	const DATE = moment().subtract(180, "d").add(x, "w").add(y, "d").format();
 
-// simpleGit().add([FILE_PATH]).commit(DATE, { "--date": DATE }).push();
+	const data = {
+		date: DATE,
+	};
+
+	jsonfile.writeFile(FILE_PATH, data, () => {
+		simpleGit()
+			.add([FILE_PATH])
+			.commit(
+				DATE,
+				{
+					"--date": DATE,
+				},
+				addCommit.bind(this, --numberOfCommits)
+			);
+	});
+};
+
+addCommit(100);
