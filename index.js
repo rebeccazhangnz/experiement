d4111cccac718671c2202a26cc39a676dbc0b7a4
Rebecca <t820134@spark.co.nz>
2022-10-07 23:08:58 +1300

    update index

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const FILE_PATH = "./data.json";
 
 async function addCommit() {
 	for (let i = 1; i < 180; i++) {
-		const DATE = moment().subtract(i, "d").format();
+		const DATE = moment().subtract(180, "d").add(i, "d").format();
 		const data = {
 			date: `${DATE + i} `,
 		};
 		for (let j = 0; j < Math.random() * 7; j++) {
-			jsonfile.writeFile(FILE_PATH, data);
+			await jsonfile.writeFile(FILE_PATH, data);
 			await simpleGit()
 				.add([FILE_PATH])
 				.commit(`${DATE + i} `, { "--date": DATE });
